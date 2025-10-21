737d43493856491353498b8221a872a870b853d8
Mate Soos <soos.mate@gmail.com>
2025-10-21 23:21:32 +0200

    Allow reading from ini file

--- kahypar/application/command_line_options.h
+++ kahypar/application/command_line_options.h
@@ -755,4 +755,26 @@ void parseIniToContext(Context& context, const std::string& ini_filename) {
     context.partition.epsilon = 0;
   }
 }
+
+void parseIniToContextString(Context& context, const std::string& str) {
+  std::istringstream file(str);
+  const int num_columns = 80;
+
+  po::variables_map cmd_vm;
+  po::options_description ini_line_options;
+  ini_line_options.add(createGeneralOptionsDescription(context, num_columns))
+  .add(createGenericOptionsDescription(context, num_columns))
+  .add(createPreprocessingOptionsDescription(context, num_columns))
+  .add(createCoarseningOptionsDescription(context, num_columns, false))
+  .add(createInitialPartitioningOptionsDescription(context, num_columns))
+  .add(createRefinementOptionsDescription(context, num_columns, false))
+  .add(createEvolutionaryOptionsDescription(context, num_columns));
+
+  po::store(po::parse_config_file(file, ini_line_options, true), cmd_vm);
+  po::notify(cmd_vm);
+
+  if (context.partition.use_individual_part_weights) {  // Note(Lars): This affects flow network sizes!
+    context.partition.epsilon = 0;
+  }
+}
 }  // namespace kahypar
